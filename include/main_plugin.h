28ba134998db7e9e1b83e7c8c83620583bae7cde
LXE97 <lxe97@proton.me>
2024-05-20 20:55:33 -0700

    get object ref from quest objective

--- include/main_plugin.h
+++ include/main_plugin.h
@@ -1,24 +1,45 @@
 #pragma once
 
 #include "SKSE/Impl/Stubs.h"
+#include "helper_game.h"
+#include "helper_math.h"
 #include "menu_checker.h"
 #include "mod_event_sink.hpp"
-#include "helper_math.h"
-#include "helper_game.h"
 
 #define _DEBUGLOG(...) \
 	if (vrmapmarkers::g_debug_print) { SKSE::log::trace(__VA_ARGS__); }
 
 namespace vrmapmarkers
 {
+	enum class MapType
+	{
+		kNone = 0,
+	};
+
 	constexpr const char* g_ini_path = "SKSE/Plugins/vrmapmarkers.ini";
 
-	extern bool          g_left_hand_mode;
-	extern bool          g_debug_print;
+	extern bool g_left_hand_mode;
+	extern bool g_debug_print;
 
 	void Init();
 
 	void OnGameLoad();
+
+	void OnMenuOpenClose(RE::MenuOpenCloseEvent const* evn);
+
+	void OnWeaponDraw(const SKSE::ActionEvent* event);
+
+	void OnEquipEvent(const RE::TESEquipEvent* event);
+
+	void UpdateMapMarkers();
+
+	std::vector<RE::NiPoint3> GetTrackedRefLocations();
+
+	RE::TESObjectREFR* GetQuestTarget(RE::BGSQuestObjective* a_obj);
+
+	void AddMarker(RE::NiPoint3 a_world, MapType a_map);
+
+	MapType GetActiveMap();
 
 	/* returns: true if config file changed */
 	bool ReadConfig(const char* a_ini_path);
--- src/main_plugin.cpp
+++ src/main_plugin.cpp
@@ -1,11 +1,14 @@
 #include "main_plugin.h"
 
+#include <chrono>
+
 namespace vrmapmarkers
 {
-	// user settings, documented in .ini
+
+	// User settings, documented in .ini
 	bool g_debug_print = true;
 
-	// settings
+	// Settings
 	bool g_left_hand_mode = false;
 
 	void Init()
@@ -13,9 +16,116 @@ namespace vrmapmarkers
 		ReadConfig(g_ini_path);
 
 		menuchecker::begin();
+
+		auto equip_sink = EventSink<RE::TESEquipEvent>::GetSingleton();
+		RE::ScriptEventSourceHolder::GetSingleton()->AddEventSink(equip_sink);
+		equip_sink->AddCallback(OnEquipEvent);
+
+		auto SKSE_action_event_sink = EventSink<SKSE::ActionEvent>::GetSingleton();
+		SKSE::GetActionEventSource()->AddEventSink(SKSE_action_event_sink);
+		SKSE_action_event_sink->AddCallback(OnWeaponDraw);
+
+		auto menu_sink = EventSink<RE::MenuOpenCloseEvent>::GetSingleton();
+		RE::UI::GetSingleton()->AddEventSink(menu_sink);
+		menu_sink->AddCallback(OnMenuOpenClose);
 	}
 
 	void OnGameLoad() { _DEBUGLOG("Load Game: reset state"); }
+
+	void OnMenuOpenClose(RE::MenuOpenCloseEvent const* evn)
+	{
+		if (!evn->opening && std::strcmp(evn->menuName.data(), "Journal Menu") == 0)
+		{
+			ReadConfig(g_ini_path);
+
+			UpdateMapMarkers();
+		}
+	}
+
+	void OnWeaponDraw(const SKSE::ActionEvent* event)
+	{
+		if (event->actor == RE::PlayerCharacter::GetSingleton()->AsReference())
+		{
+			switch (event->type.get())
+			{
+			case SKSE::ActionEvent::Type::kBeginSheathe:
+			case SKSE::ActionEvent::Type::kEndSheathe:
+			case SKSE::ActionEvent::Type::kBeginDraw:
+			case SKSE::ActionEvent::Type::kEndDraw:
+				UpdateMapMarkers();
+			default:
+				break;
+			}
+		}
+	}
+
+	void OnEquipEvent(const RE::TESEquipEvent* event)
+	{
+		if (event->actor &&
+			event->actor.get() == RE::PlayerCharacter::GetSingleton()->AsReference())
+		{
+			if (auto item = RE::TESForm::LookupByID(event->baseObject); item->IsWeapon())
+			{
+				UpdateMapMarkers();
+			}
+		}
+	}
+
+	void UpdateMapMarkers()
+	{
+		if (auto map = GetActiveMap(); map != MapType::kNone)
+		{
+			auto coords = GetTrackedRefLocations();
+
+			for (auto& obj : coords) { AddMarker(obj, map); }
+		}
+	}
+
+	MapType GetActiveMap() { return MapType::kNone; }
+
+	std::vector<RE::NiPoint3> GetTrackedRefLocations()
+	{
+		std::vector<RE::NiPoint3> result;
+
+		for (auto& instance : RE::PlayerCharacter::GetSingleton()->objectives)
+		{
+			auto objective = instance.Objective;
+			if (objective->ownerQuest->IsActive() &&
+				objective->state == RE::QUEST_OBJECTIVE_STATE::kDisplayed)
+			{
+				if (auto ref = GetQuestTarget(objective)) { result.push_back(ref->GetPosition()); }
+			}
+		}
+
+		return result;
+	}
+
+	RE::TESObjectREFR* GetQuestTarget(RE::BGSQuestObjective* a_obj)
+	{
+		if (a_obj->targets)
+		{
+			if (auto target = *(a_obj->targets))
+			{
+				auto alias = target->alias;
+				auto quest = a_obj->ownerQuest;
+				if (auto it = quest->refAliasMap.find(alias); it != quest->refAliasMap.end())
+				{
+					auto refhandle = it->second;
+					if (auto refptr = refhandle.get()) { return refptr.get(); }
+				}
+			}
+		}
+		return nullptr;
+	}
+
+	void AddMarker(RE::NiPoint3 a_world, MapType a_map)
+	{
+		// Check if marker location is inside currently equipped map
+
+		// Get icon transform relative to hand
+
+		// Add icon
+	}
 
 	bool ReadConfig(const char* a_ini_path)
 	{
@@ -60,5 +170,4 @@ namespace vrmapmarkers
 		}
 		return false;
 	}
-
 }
